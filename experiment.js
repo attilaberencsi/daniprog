a83a4ef5066716d39f36517864fa87cde1f42073
Attila Berencsi <attilaberencsi@gmail.com>
2023-04-24 15:54:44 +0200

    first commit

---
+++ experiment.js
@@ -0,0 +1,7 @@
+console.log("goal")
+var grhu = 5345 * 12345;
+console.log(grhu)
+console.log("pist")
+console.log("lo code no code");
+var szavak =["szombat","gamer dani","videozas"]
+console.table(szavak)
---
+++ readme.md
@@ -0,0 +1,3 @@
+![](assets/20230424_155106_image.png)
+
+Dani 5-öst adott magának, le is bukott
